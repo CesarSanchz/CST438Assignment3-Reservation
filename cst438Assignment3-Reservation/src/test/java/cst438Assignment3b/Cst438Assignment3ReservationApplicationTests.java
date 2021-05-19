7046a59cb8ed0aecbc5788635049b225d2c2e02a
cesarsanchez <cesarsanchez@DESKTOP-JQV67KS.home>
2021-05-18 17:47:41 -0700

    Java files for peer-review

---
+++ cst438Assignment3-Reservation/src/test/java/cst438Assignment3b/Cst438Assignment3ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438Assignment3b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Assignment3ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
